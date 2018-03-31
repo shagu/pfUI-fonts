b5679a3a5362911b0bf70cb34df401d7642fb140
shagu <ericmauser@googlemail.com>
2018-04-01 00:12:42 +0200

    rename: pfUI-fonts
    * now includes several fonts
    * all credits to the google font project

--- pfUI-font-roboto.lua
+++
@@ -1,6 +0,0 @@
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-font-roboto\\fonts\\Roboto-Black.ttf:Roboto (Black)")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-font-roboto\\fonts\\Roboto-Bold.ttf:Roboto (Bold)")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-font-roboto\\fonts\\Roboto-Light.ttf:Roboto (Light)")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-font-roboto\\fonts\\Roboto-Medium.ttf:Roboto (Medium)")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-font-roboto\\fonts\\Roboto-Regular.ttf:Roboto (Regular)")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-font-roboto\\fonts\\Roboto-Thin.ttf:Roboto (Thin)")
