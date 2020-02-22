21000056503a71c349584c34b159e1a04974de78
shagu <ericmauser@googlemail.com>
2020-02-22 11:42:07 +0100

    fonts: add support for dynamic folder names

--- pfUI-fonts.lua
+++ pfUI-fonts.lua
@@ -1,13 +1,24 @@
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-fonts\\fonts\\BalooBhaina.ttf:BalooBhaina")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-fonts\\fonts\\Bungee.ttf:Bungee")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-fonts\\fonts\\CaesarDressing.ttf:CaesarDressing")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-fonts\\fonts\\CoveredByYourGrace.ttf:CoveredByYourGrace")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-fonts\\fonts\\JotiOne.ttf:JotiOne")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-fonts\\fonts\\LondrinaSolid.ttf:LondrinaSolid")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-fonts\\fonts\\NovaFlat.ttf:NovaFlat")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-fonts\\fonts\\Roboto.ttf:Roboto")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-fonts\\fonts\\SedgwickAveDisplay.ttf:SedgwickAveDisplay")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-fonts\\fonts\\ShareBold.ttf:ShareBold")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-fonts\\fonts\\Share.ttf:Share")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-fonts\\fonts\\Sniglet.ttf:Sniglet")
-table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-fonts\\fonts\\SquadaOne.ttf:SquadaOne")
+local addonpath
+local tocs = { "", "-master", "-tbc", "-wotlk" }
+for _, name in pairs(tocs) do
+  local current = string.format("pfUI-fonts%s", name)
+  local _, title = GetAddOnInfo(current)
+  if title then
+    addonpath = "Interface\\AddOns\\" .. current
+    break
+  end
+end
+
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\BalooBhaina.ttf:BalooBhaina")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\Bungee.ttf:Bungee")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\CaesarDressing.ttf:CaesarDressing")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\CoveredByYourGrace.ttf:CoveredByYourGrace")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\JotiOne.ttf:JotiOne")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\LondrinaSolid.ttf:LondrinaSolid")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\NovaFlat.ttf:NovaFlat")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\Roboto.ttf:Roboto")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\SedgwickAveDisplay.ttf:SedgwickAveDisplay")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\ShareBold.ttf:ShareBold")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\Share.ttf:Share")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\Sniglet.ttf:Sniglet")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\SquadaOne.ttf:SquadaOne")
